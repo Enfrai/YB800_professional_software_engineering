4e3875e4d634acf5034891d413741029d49200a3
Enfrai <mengmeng.michael@outlook.com>
2026-08-08 09:29:22 +1200

    transform bmi function to class

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Shuohui Liu (Michael) @YooBee learning
 ## Week 1-2
 - [Fibonacci and factorial calculation](print_fibonacci.py)
 - [Problmes of average, sum, production and bmi](week_1_to_2_exercise_1.py)
+
+## Week 2
+- [Transform BMI to class](week_2_activity_1_update_bmi.py)
--- week_2_activity_1_update_bmi.py
+++ week_2_activity_1_update_bmi.py
@@ -1,2 +1,22 @@
+from week_1_to_2_exercise_1 import on_value_error
+
 class BMI:
-  
+    def __init__(self, weight: float, height: float):
+        self.weight = weight
+        self.height = height
+
+    def calculate(self) -> float:
+        return self.weight * self.height ** 2
+
+def main():
+    try:
+        weight = float(input('Please input the weight(kg): '))
+        height = float(input('Please input the height(m): '))
+
+        bmi = BMI(weight, height)
+        print(f'The BMI is {bmi.calculate()}')
+    except ValueError:
+        on_value_error(prompt='Please provide weight and height in FLOAT value', f=main)
+
+if __name__ == '__main__':
+    main()
