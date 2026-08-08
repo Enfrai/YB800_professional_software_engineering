0bf3abd41a6eaca1edf4de305652361c6eec17bd
Michael4691 <mengmeng.michael@outlook.com>
2026-08-08 20:15:29 +1200

    make comments step by step.

--- week_2_activity_2_update_to_oop_Word_G.py
+++ week_2_activity_2_update_to_oop_Word_G.py
@@ -10,21 +10,38 @@ Share your GitHub link by tomorrow at 8:00 AM
 '''
 
 def get_random_word():
-    
+    '''
+    Randomly pick up and return one word from a list of constant words.
+    '''
+
+    # To define a data set of words
     words = [
         "python", "variable", "function", "iterator", "notebook",
         "pipeline", "dataset", "computer", "research", "analytics"
     ]
+    # Randomly pick up a word from the data set and return it
     return random.choice(words)
 
 def make_blanks(word):
-    
+    '''
+    Relace each letter in the word with an underscore, 
+    store them in a list and return.
+    '''
     return ["_" for _ in word]
 
 def prompt_for_letter(used_letters):
+    '''
+    Lead user to input a letter, 
+    and validate the input to ensure it is a single letter and
+    not already used.
+    '''
 
     while True:
+        # Grab an input from the user with wish for a letter
         guess = input("Guess a letter: ").strip().lower()
+
+        # Validate the input to ensure it is a single letter and
+        # not already used
         if len(guess) != 1 or guess not in string.ascii_lowercase:
             print(" → Please enter a single A-Z letter.")
             continue
@@ -34,6 +51,12 @@ def prompt_for_letter(used_letters):
         return guess
 
 def reveal_letters(word, blanks, letter):
+    '''
+    Try to verify if the letter is used for the spelling of the word.
+    If it is, replace the blank '_' with the letter for any time 
+    this letter appears in the word and return true.
+    And if not, return false.
+    '''
 
     found_any = False
     for i, ch in enumerate(word):
@@ -43,42 +66,59 @@ def reveal_letters(word, blanks, letter):
     return found_any
 
 def all_blanks_filled(blanks):
-   
-    return "_" not in blanks
+   '''
+   Check if all blanks are filled with letters.
+   If yes, return true. Otherwise, return false.
+   '''
+   return "_" not in blanks
 
 def play_game(max_lives=6):
-    
+    '''
+    Start to play a word guessing game. 
+    The user will be asked to guess what letters are contained 
+    in a random word with a limited number of lives, default to 6.
+    '''
+
+    # Randomly generate a word.
     secret = get_random_word()
+    # Init a list of blanks to represent the letters in the word.
     blanks = make_blanks(secret)
+    # Maximum tries for misses, default to 6.
     lives = max_lives
+    # Init a set to store what letters have been tried by the user.
     used = set()
 
     print("\nWelcome to Word Guessing!")
     print(f"The word has {len(secret)} letters.")
+
+    # Show blanks to the user for the first time.
     print(" ".join(blanks))
 
     while True:
-        # Ask the user to guess a letter
+        # Ask the user to successfully guess a letter
         guess = prompt_for_letter(used)
         used.add(guess)
 
-        # Is the guessed letter in the word?
+        # To check if the guessed letter is in the word, and if yes, reveal the letter in the blanks.
         if reveal_letters(secret, blanks, guess):
+            # The letter is matched and revealed, and show the gussed result.
             print("\n Well done, Nice job! You found a letter.")
             print(" ".join(blanks))
-            # Are all blanks filled?
+            # To check if all blanks are filled with letters, 
+            # and if yes, the user wins the game and jump out the loop.
             if all_blanks_filled(blanks):
                 print("\n Congratulation! You guessed the word!")
                 print(f"Word: {secret}")
                 print("GAME OVER")
                 break
         else:
-            # Lose a life
+            # The letter is not matched, maximum tries for misses is reduced by 1,
+            # and show the already guessed result.
             lives -= 1
             print(f"\nNope. You lose a life. Lives left: {lives}")
             print(" ".join(blanks))
 
-            # Have they run out of lives?
+            # Jump out the loop, and show the correct word if the user is out of lives.
             if lives <= 0:
                 print("\n Out of lives & Sad story!")
                 print(f"The word was: {secret}")
@@ -89,4 +129,5 @@ def play_game(max_lives=6):
 
 
 if __name__ == "__main__":
+    # Main entrance.
     play_game()
