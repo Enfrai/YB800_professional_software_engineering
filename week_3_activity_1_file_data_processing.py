2baf62d713cb25a83ad37a0f7fb86baaf9cdb37f
Enfrai <mengmeng.michael@outlook.com>
2026-08-15 09:54:08 +1200

    Update week_3_activity_1_file_data_processing.py

--- week_3_activity_1_file_data_processing.py
+++ week_3_activity_1_file_data_processing.py
@@ -5,10 +5,10 @@ iris = fetch_ucirepo(id=53)
 x = iris.data.features
 y = iris.data.targets
 
+print(iris.metadata)
+print(iris.variables)
+
 print(f'The total number of records is {len(x)}')
-print(f'The total number of different flower available is {y['class'].nunique()}')
-print(f'The names of all diferent flowers are: {y['class'].unique().tolist()}')
+print(f'The total number of different flower available is {y["class"].nunique()}')
+print(f'The names of all diferent flowers are: {y["class"].unique().tolist()}')
 
-# print(iris.metadata)
-
-# print(iris.variables)
