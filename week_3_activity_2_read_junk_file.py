72fb9812db264f948a307f440a83024e5252cbbc
Enfrai <mengmeng.michael@outlook.com>
2026-08-15 20:42:58 +1200

    Update week_3_activity_2_read_junk_file.py

--- week_3_activity_2_read_junk_file.py
+++ week_3_activity_2_read_junk_file.py
@@ -6,18 +6,49 @@ Open, read, and process the attached data file. Use the attached `junk.txt` file
 4. Save the processed file. Share your GitHub repository link here once you have completed the task.
 '''
 
-with open('resources/junk.txt', 'r+') as file:
-    lines = file.readlines()
-    print(f'Total number of lines is {len(lines)}')
+#SOLUTION 1
+def solution_1():
+    with open('resources/junk.txt', 'r') as file:
+        lines = file.readlines()
 
-    lines.append('text file nanalyssis')
+    # 1. Calculate and report the total number of lines in the file.
+    print(f'The total number of lines is {len(lines)}')
 
-    up_lines = []
+    # 2. Add a new line at the end of the file containing exactly: `text file nanalyssis`
+    new_line = 'text file nanalyssis\n'
+    lines.append(new_line)
+    with open('resources/junk.txt', 'a') as file:
+        file.write(new_line)
+
+    # 3. Convert all text in the `junk.txt` file to lowercase.
+    lower_lines = []
     for line in lines:
-        up_lines.append(line.upper())
+        lower_lines.append(line.lower())
 
-    up_lines.append('\n')
+    # 4. Save the processed file. Share your GitHub repository link here once you have completed the task.
+    with open('resources/junk.txt', 'w') as file:
+        file.writelines(lower_lines)
 
-    file.writelines(up_lines)
 
-    file.close()
+# SOLUTION 2
+def solution_2():
+    with open('resources/junk.txt', 'r+') as file:
+        lines = file.readlines()
+        print(f'Total number of lines is {len(lines)}')
+
+        lines.append('text file nanalyssis')
+
+        lower_lines = []
+        for line in lines:
+            lower_lines.append(line.lower())
+
+        lower_lines.append('\n')
+        # print(lower_lines)
+
+        file.seek(0)
+        file.writelines(lower_lines)
+        file.truncate()
+
+if __name__ == '__main__':
+    # solution_1()
+    solution_2()
