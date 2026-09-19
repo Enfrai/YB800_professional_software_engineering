57f07ab62275f537db0f3ef3ae4461debc894562
Enfrai <shuohui.liu@yahoo.com>
2026-09-20 09:08:47 +1200

    factory pattern test

--- week_8_exercise_1_factory_pattern/week_8_exercise_1_factory_pattern.py
+++ week_8_exercise_1_factory_pattern/week_8_exercise_1_factory_pattern.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Pizza:
     def prepare(self):
         print('ordered pizza')
@@ -21,9 +23,51 @@ class OrderFactory:
         elif order == 'pasta':
             return Pasta()
 
+#######################################################
+
+class NotificationMethod(ABC):
+    @abstractmethod
+    def notify(self):
+        pass
+
+class Email(NotificationMethod):
+    def notify(self):
+        print('through email')
+
+class SMS(NotificationMethod):
+    def notify(self):
+        print('through sms')
+
+class Push(NotificationMethod):
+    def notify(self):
+        print('through push')
+
+class NotificationFactory(ABC):
+    @abstractmethod
+    def create_notification(self):
+        pass
+
+class EmailFactory(NotificationFactory):
+    def create_notification(self):
+        return Email()
+
+class SMSFactory(NotificationFactory):
+    def create_notification(self):
+        return SMS()
+
+class PushFactory(NotificationFactory):
+    def create_notification(self):
+        return Push()
+    
+
+#######################################################
+
 def main():
     order = OrderFactory().create("pizza")
     order.prepare()
+
+    push = SMSFactory()
+    push.create_notification().notify()
 
 
 if __name__ == '__main__':
